de1d257b08b855c79859f6d456cd93000c9e4829
Wesley Farias <wesleysousukee@gmail.com>
2022-04-03 18:51:53 -0300

    Ajusta estilo de codigo

--- Encapsulamento/Produto.cs
+++ Encapsulamento/Produto.cs
@@ -1,71 +1,57 @@
 ﻿using System;
 using System.Globalization;
 
-namespace Encapsulamento
-{
-    class Produto
-    {
+namespace Encapsulamento {
+    class Produto {
         // Atributos
         private string _nome;
         private double _preco;
         private int _quantidade;
 
         // Construtores
-        public Produto()
-        {
+        public Produto() {
             _quantidade = 5;
         }
-        public Produto(string nome, double preco) : this()
-        {
+        public Produto(string nome, double preco) : this() {
             _nome = nome;
             _preco = preco;
         }
-        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
-        {
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) {
             _quantidade = quantidade;
         }
 
         // Properties
-        public string Nome
-        {
+        public string Nome {
             get { return _nome; }
-            set
-            {
-                if (value != null && value.Length > 1)
-                {
+            set {
+                if (value != null && value.Length > 1) {
                     _nome = value;
                 }
             }
         }
 
-        public double Preco
-        {
+        public double Preco {
             get { return _preco; }
         }
 
-        public int Quantidade
-        {
+        public int Quantidade {
             get { return _quantidade; }
 
         }
 
         // Métodos
-        public double QuantidadeTotalEmEstoque()
-        {
+        public double QuantidadeTotalEmEstoque() {
             return _quantidade * _preco;
         }
-        public int AdicionarProduto(int quantidade)
-        {
+        public int AdicionarProduto(int quantidade) {
             return _quantidade += quantidade;
         }
-        public int RemoverProduto(int quantidade)
-        {
+        public int RemoverProduto(int quantidade) {
             return _quantidade -= quantidade;
         }
 
         // ToString
-        public override string ToString()
-        {
+        public override string ToString() {
             return $"Dados do produto: {_nome}, " +
                    $"R$ {_preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
                    $"{_quantidade} unidades, " +
--- Encapsulamento/Program.cs
+++ Encapsulamento/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System.Globalization;
 
-namespace Encapsulamento
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Encapsulamento {
+    class Program {
+        static void Main(string[] args) {
             Produto produto = new Produto("TV", 500.00, 10);
             produto.Nome = "TV";
             produto.AdicionarProduto(10);
